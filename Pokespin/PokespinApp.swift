17ad754d152b526bc76f7757f30c0b0cbc02d92e
Ivan Almada <ivan.almada@me.com>
2023-11-27 11:29:33 +0100

    Initial commit with pokemon images and landing screen in SwiftUI

---
+++ Pokespin/PokespinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokespinApp.swift
+//  Pokespin
+//
+//  Created by Iván Almada on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PokespinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
