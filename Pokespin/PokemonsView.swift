07a570db2878e834f1a66af5a68022def238e3fc
Ivan Almada <ivan.almada@me.com>
2023-11-27 16:23:26 +0100

    Create SwiftUI View that shows a pokemon image if the pokemon number is divisible by 2 or otherwise a text with the pokemon number

--- Pokespin/PokemonsView.swift
+++ Pokespin/PokemonsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct Pokemon {
     var number: String
+    var isUnlocked: Bool
 }
 
 struct PokemonsView: View {
@@ -16,7 +17,7 @@ struct PokemonsView: View {
     var pokemons: [Pokemon] {
         var result = [Pokemon]()
         for i in 0..<18 {
-            result.append(Pokemon(number: "\(i)"))
+            result.append(Pokemon(number: "\(i)", isUnlocked: (i%2 == 0)))
         }
         return result
     }
@@ -26,9 +27,17 @@ struct PokemonsView: View {
             ForEach (0..<6) { row in
                 GridRow {
                     ForEach(0..<3) { column in
-                        Text("\(pokemons[3*row+column].number)")
-                            .font(.largeTitle)
-                            .padding()
+                        var index = 3 * row + column
+                        var pokemon = pokemons[index]
+                        if pokemon.isUnlocked {
+                            Image(String(index+1))
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        } else {
+                            Text("\(pokemons[index].number)")
+                                .font(.largeTitle)
+                                .padding()
+                        }
                     }
                 }
             }
