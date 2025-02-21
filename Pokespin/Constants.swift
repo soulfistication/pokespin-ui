aaba29f5dcb8578f5b19a8606046e57c437e19b7
Ivan Almada <ivan.almada@me.com>
2025-02-20 20:52:59 -0600

    Add Storyboard and Slot Machine View Controller

--- Pokespin/Constants.swift
+++ Pokespin/Constants.swift
@@ -1,15 +1,29 @@
 //
 //  Constants.swift
-//  Pokespin
+//  PokeSpin
 //
-//  Created by Montserrat Arroyo on 2/20/25.
+//  Created by Ivan Almada on 18/01/2018.
+//  Copyright © 2018 Ivan. All rights reserved.
 //
+
+import Foundation
 
 struct Constants {
 
     static let numberOfPokemonsDisplayed = 18
     static let numberOfColumnsInSlotMachine = 3
     static let numberOfRowsInSlotMachine = 36
+
+    enum SegueIdentifier: String {
+        case openSlotMachine = "ELSegueIdentifierOpenSlotMachine";
+        case openSuccess = "ELSegueIdentifierOpenSuccessSlotMachine";
+        case openPokemonUnlocked = "ELSegueIdentifierOpenPokemonUnlocked";
+        case openPokeDex = "ELSegueIdentifierOpenPokeDex";
+    }
+
+    enum CellIdentifiers: String {
+        case pokemonCollectionViewCell = "ELCellIdentifierPokemonCollectionViewCell"
+    }
 
     enum ApiURL: String {
         case baseURL = "https://pokeapi.co";
--- Pokespin/SlotMachineViewController.swift
+++ Pokespin/SlotMachineViewController.swift
@@ -1,40 +1,30 @@
 //
 //  SlotMachineViewController.swift
-//  Pokespin
+//  PokeSpin
 //
-//  Created by Montserrat Arroyo on 2/20/25.
+//  Created by Ivan Almada on 18/01/2018.
+//  Copyright © 2018 Ivan. All rights reserved.
 //
 
 import UIKit
 
-class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    
-    var slotMachinePickerView: UIPickerView
-    
-    // MARK: - Init
-    
-    required init?(coder: NSCoder) {
-        slotMachinePickerView = UIPickerView()
-        slotMachinePickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        slotMachinePickerView.translatesAutoresizingMaskIntoConstraints = false
-        super.init(coder: coder)
-        slotMachinePickerView.delegate = self
-        slotMachinePickerView.dataSource = self
-    }
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        fatalError("Not supported")
-    }
-    
-    init() {
-        slotMachinePickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
-        slotMachinePickerView.translatesAutoresizingMaskIntoConstraints = false
-        super.init(nibName: nil, bundle: nil)
-        slotMachinePickerView.delegate = self
-        slotMachinePickerView.dataSource = self
-        
-    }
-    
+@objc protocol ScreenDismissable: NSObjectProtocol {
+    func screenDismissed()
+}
+
+class SlotMachineViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, ScreenDismissable {
+
+    var pokemonNumber: Int = 0
+    weak var delegate: ScreenDismissable?
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var pokemonNumberLabel: UILabel!
+    @IBOutlet weak var slotMachinePickerView: UIPickerView!
+    @IBOutlet weak var wonLabel: UILabel!
+    @IBOutlet weak var wonImageView: UIImageView!
+    @IBOutlet weak var spinSlotButton: UIButton!
+
     // MARK: - UIViewController
 
     override func viewDidLoad() {
@@ -45,12 +35,23 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
     // MARK: - Setup UI
 
     func setupUI() {
+        pokemonNumberLabel.text = String(pokemonNumber)
         view.backgroundColor = UIColor.creamyBlue
         slotMachinePickerView.selectRow(4, inComponent: 0, animated: true)
         slotMachinePickerView.selectRow(4, inComponent: 1, animated: true)
         slotMachinePickerView.selectRow(4, inComponent: 2, animated: true)
     }
-    
+
+    // MARK: - IBActions
+
+    @IBAction func closeButtonTapped(sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func spinSlotMachine(sender: AnyObject) {
+        spin()
+    }
+
     // MARK: - UIPickerView Data Source
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -81,6 +82,70 @@ class SlotMachineViewController: UIViewController, UIPickerViewDelegate, UIPicke
         }
         return "💊"
     }
+
+    // MARK: - Screen Dismissable
+
+    func screenDismissed() {
+        dismiss(animated: true) { [weak self] in
+            self?.delegate?.screenDismissed()
+        }
+    }
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == Constants.SegueIdentifier.openSuccess.rawValue {
+//            let successViewController = segue.destination as! SuccessViewController
+//            successViewController.pokemonNumber = pokemonNumber
+//            successViewController.delegate = self
+//        }
+    }
+    
+    // MARK: - Logic
+    func spin() {
+        spinSlotButton.isEnabled = false
+        
+        let firstComponentRandomNumber = Int.random(in: 12...24)
+        let secondComponentRandomNumber = Int.random(in: 12...24)
+        let thirdComponentRandomNumber = Int.random(in: 12...24)
+
+        slotMachinePickerView.selectRow(firstComponentRandomNumber, inComponent: 0, animated: true)
+        slotMachinePickerView.selectRow(secondComponentRandomNumber, inComponent: 1, animated: true)
+        slotMachinePickerView.selectRow(thirdComponentRandomNumber, inComponent: 2, animated: true)
+
+        let firstSymbol = slotSymbol(row: firstComponentRandomNumber)
+        let secondSymbol = slotSymbol(row: secondComponentRandomNumber)
+        let thirdSymbol = slotSymbol(row: thirdComponentRandomNumber)
+
+        let firstHit = firstSymbol == secondSymbol
+        let secondHit = secondSymbol == thirdSymbol
+        let thirdHit = firstSymbol == thirdSymbol
+
+        let successHit = firstHit && secondHit
+        //let successHit = true // Make it always win
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
+            if successHit {
+                self?.wonLabel.isHidden = false
+                self?.wonImageView.isHidden = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: { [weak self] in
+                    self?.performSegue(withIdentifier: Constants.SegueIdentifier.openSuccess.rawValue, sender: nil)
+                })
+            } else {
+                var message = "You lost! Please try again."
+                if firstHit || secondHit || thirdHit {
+                    message = "You almost won! Please try again"
+                }
+                let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default, handler: { [weak self] (action) in
+                    self?.dismiss(animated: true, completion: nil)
+                })
+                alertController.addAction(action)
+                self?.show(alertController, sender: nil)
+            }
+        }
+    }
+
 }
 
 extension UIColor {
